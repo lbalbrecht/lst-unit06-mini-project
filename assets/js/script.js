e1cc0c55c8fd34d057b645d271a95a5f4d5606ab
sungjk77 <80664973+sungjk77@users.noreply.github.com>
2021-04-02 13:30:26 -0700

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,66 @@ var articleSubject;
 var articleDescription;
 var articleLink;
 
+
+// function displayArticles(data) {
+//     console.log(data);
+//     console.log(data.results.length);
+//   for (let i = 0; i < data.results.length; i++) {
+//     articleTitle = data.results[i].title; 
+//     //console.log(articleTitle);
+//     //console.log(articleAuthors);
+//     articleDate = data.results[i].date;
+//     articleSubject = data.results[0].subject.join(", ");
+//     articleDescription = data.results[0].description[0];
+//     articleLink = data.results[0].url;
+//   }
+// }
+
+var articleCardEL = document.querySelector('#article-card');
+var articleTitle = document.getElementById("article-title");
+var articleDate = document.getElementById("article-date");
+var articleSubject = document.getElementById("article-subject");
+var articleLink = document.getElementById("article-link");
+
+var displayArticle = function (article) {
+    if (article.length === 0) {
+      articleCardEl.textContent = 'No articles found.';
+      return;
+    }
+
+    for (var i = 0; i < article.length; i++) {
+      var articleTitle = article.results[i].title;
+      var articleDate = article.results[i].date;
+      var articleSubject = article.results[i].description[0];
+      var articleLink = data.results[i].url;
+
+    // testing variables
+      // var articleTitle = "article.results[i].title";
+      // var articleDate = "article.results[i].date";
+      // var articleSubject = "article.results[i].description[0]";
+      // var articleLink = "data.results[i].url";
+
+      var varText =  
+      `
+      <div class="col s12">
+      <div class="card blue-grey darken-1">
+          <div class="card-content white-text">
+              <span class="article-title" id="article-title">`+articleTitle+`</span>
+              <p id="article-date">`+articleDate+`</p>
+              <p id="article-subject">`+articleSubject+`</p>
+              <p id="article-description">Description</p>
+          </div>
+          <div class="card-action">
+              <a href="#`+articleLink+`" class="waves-effect waves-light btn-small" id="read-more">Read More</button> </a>
+          </div>
+      </div>
+  </div>
+    `;
+  
+      articleCardEl.innerHTML = varText;
+    }
+};
+
 function formSubmitHandler(term, format) {
   if (!format) {
     format = "search";
@@ -19,25 +79,12 @@ function formSubmitHandler(term, format) {
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
-        displayArticles(data);
+        displayArticle(data);
       });
     }
   });
 }
 
-function displayArticles(data) {
-    console.log(data);
-    console.log(data.results.length);
-  for (let i = 0; i < data.results.length; i++) {
-    articleTitle = data.results[i].title; 
-    //console.log(articleTitle);
-    //console.log(articleAuthors);
-    articleDate = data.results[i].date;
-    articleSubject = data.results[0].subject.join(", ");
-    articleDescription = data.results[0].description[0];
-    articleLink = data.results[0].url;
-  }
-}
 
 formSubmitHandler("constitution");
 
@@ -45,3 +92,4 @@ formSubmitHandler("constitution");
 //   "submit",
 //   formSubmitHandler(searchTerm, searchFormat)
 // );
+
